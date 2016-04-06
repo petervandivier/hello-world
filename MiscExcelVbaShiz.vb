2716d5145e6e82f3b23491352340ff3a97118f47
petervandivier <peter.v@guaranteedrate.com>
2016-04-05 22:01:54 -0500

    add Levenshtein function

--- MiscExcelVbaShiz.vb
+++ MiscExcelVbaShiz.vb
@@ -221,3 +221,47 @@ Sub highlightext()
 	End If
 
 End Sub
+
+
+'copied from http://stackoverflow.com/questions/4243036/levenshtein-distance-in-excel
+'variants and optimized versions available at the link above
+Option Explicit
+Public Function Levenshtein(s1 As String, s2 As String)
+
+Dim i As Integer
+Dim j As Integer
+Dim l1 As Integer
+Dim l2 As Integer
+Dim d() As Integer
+Dim min1 As Integer
+Dim min2 As Integer
+
+l1 = Len(s1)
+l2 = Len(s2)
+ReDim d(l1, l2)
+For i = 0 To l1
+    d(i, 0) = i
+Next
+For j = 0 To l2
+    d(0, j) = j
+Next
+For i = 1 To l1
+    For j = 1 To l2
+        If Mid(s1, i, 1) = Mid(s2, j, 1) Then
+            d(i, j) = d(i - 1, j - 1)
+        Else
+            min1 = d(i - 1, j) + 1
+            min2 = d(i, j - 1) + 1
+            If min2 < min1 Then
+                min1 = min2
+            End If
+            min2 = d(i - 1, j - 1) + 1
+            If min2 < min1 Then
+                min1 = min2
+            End If
+            d(i, j) = min1
+        End If
+    Next
+Next
+Levenshtein = d(l1, l2)
+End Function
