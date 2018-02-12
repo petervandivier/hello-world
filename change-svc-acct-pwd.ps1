7deec3cc74f3ecf08c1986e8d01e3182a1d75b99
petervandivier <petervandivier@gmail.com>
2018-02-12 20:02:48 +0000

    rotate svc account script fix

--- change-svc-acct-pwd.ps1
+++ change-svc-acct-pwd.ps1
@@ -1,6 +1,7 @@
-﻿#https://mcpmag.com/articles/2015/01/22/password-for-a-service-account-in-powershell.aspx
+﻿#run from an admin session
+#https://mcpmag.com/articles/2015/01/22/password-for-a-service-account-in-powershell.aspx
 $svcAcct="yolo\all.day"
-$Service = (Get-WmiObject -Class Win32_Service) | Where-Object startname -eq '$svcAcct'
+$Service = (Get-WmiObject -Class Win32_Service) | Where-Object startname -eq $svcAcct
 $Service | Select Name, StartName, State
 $Password = Read-Host -Prompt "Enter password for $svcAcct" -AsSecureString
 #required to decrypt PWD from securestring before applying
