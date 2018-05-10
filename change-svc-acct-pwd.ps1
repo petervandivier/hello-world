e33204b72caa8c0bcdff1e415344698e7970657c
Peter Vandivier <petervandivier@gmail.com>
2018-05-10 11:21:21 +0100

    Update change-svc-acct-pwd.ps1

--- change-svc-acct-pwd.ps1
+++ change-svc-acct-pwd.ps1
@@ -1,14 +1,19 @@
-﻿#run from an admin session
-#https://mcpmag.com/articles/2015/01/22/password-for-a-service-account-in-powershell.aspx
-$svcAcct="yolo\all.day"
+# run from an admin session
+# https://mcpmag.com/articles/2015/01/22/password-for-a-service-account-in-powershell.aspx
+$svcAcct = whoami
 $Service = (Get-WmiObject -Class Win32_Service) | Where-Object startname -eq $svcAcct
 $Service | Select Name, StartName, State
+
 $Password = Read-Host -Prompt "Enter password for $svcAcct" -AsSecureString
-#required to decrypt PWD from securestring before applying
+# required to decrypt PWD from securestring before applying
 $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($Password)
 $Password = [system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR)
 $Service.Change($Null,$Null,$Null,$Null,$Null,$Null,$Null,$Password,$Null,$Null,$Null)
+
+# s/b 5 if already stopped
 $Service.StopService().ReturnValue
-$Service.StartService().ReturnValue 
+
+# s/b 0
+$Service.StartService().ReturnValue
 Remove-Variable Password,BSTR
 [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR) 
