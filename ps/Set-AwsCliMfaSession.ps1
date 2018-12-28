beba4b1df26b49e0f4224716c9d0c435664e5e26
petervandivier <petervandivier@gmail.com>
2018-12-28 13:25:51 +0000

    Invoke-ImDumb -NoClobber -Env:Vars

--- ps/Set-AwsCliMfaSession.ps1
+++ ps/Set-AwsCliMfaSession.ps1
@@ -1,15 +1,15 @@
 [cmdletbinding()]param(
      [Parameter(Mandatory=$true)]$AWS_MFA_Code
-    ,$profile = "default"
+    ,$AwsProfile = "default"
 )
 
-$serial_number = (aws sts get-caller-identity --profile $profile --output text --query 'Arn').Replace('user','mfa')
+$serial_number = (aws sts get-caller-identity --profile $AwsProfile --output text --query 'Arn').Replace('user','mfa')
 
-$json = aws sts get-session-token --profile $profile --serial-number $serial_number --token-code $AWS_MFA_Code
+$json = aws sts get-session-token --profile $AwsProfile --serial-number $serial_number --token-code $AWS_MFA_Code
 $obj = $json | ConvertFrom-Json
 
 Set-item -path env:AWS_ACCESS_KEY_ID -value $obj.Credentials.AccessKeyId
 Set-item -path env:AWS_SESSION_TOKEN -value $obj.Credentials.SessionToken
 Set-item -path env:AWS_SECRET_ACCESS_KEY -value $obj.Credentials.SecretAccessKey
 
-Write-Verbose "Session credential set for profile [$profile]" -Verbose
+Write-Verbose "Session credential set for profile [$AwsProfile]" -Verbose
